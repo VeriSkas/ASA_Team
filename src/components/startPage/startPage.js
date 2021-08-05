20893543d2c10e17a54ca1bd74c4b346db67d5dc
Светлана Веракса <92_sveto4ka@mail.ru>
2021-08-05 16:23:38 +0300

    startPage table

--- src/components/startPage/startPage.js
+++ src/components/startPage/startPage.js
@@ -2,20 +2,30 @@ import { routes } from '../../shared/constants/routes';
 
 export const startPageStyle = () => {
     const bgImg = document.getElementById('bg');
-    const moonImg = document.getElementById('moon');
-    const montainImg = document.getElementById('montain');
-    const roadImg = document.getElementById('road');
+    const typewriter = document.querySelector('#typewriter');
+    const openBook = document.querySelector('#openBook');
+    const book = document.querySelector('#book');
+    const eraser = document.querySelector('#eraser');
+    const glasses = document.querySelector('#glasses');
+    const coffie = document.querySelector('#coffie');
+    const bottomImg = document.querySelector('#bottomImg');
     const textTitle = document.querySelector('.main_startPage_header-text');
     const startPageBtn = document.querySelector('.startPageBtn');
 
     window.addEventListener('scroll', () => {
         let value = window.scrollY;
 
-        bgImg.style.top = `${value*0.5}px`;
-        moonImg.style.left = `-${value*0.5}px`;
-        montainImg.style.top = `-${value*0.15}px`;
-        roadImg.style.top = `${value*0.15}px`;
-        textTitle.style.top = `${value}px`;
+        bgImg.style.top = `${value*0.15}px`;
+        typewriter.style.top = `-${value*0.8}px`;
+        openBook.style.left = `${value*0.8}px`;
+        book.style.left = `-${value*0.8}px`;
+        glasses.style.top = `${value*0.8}px`;
+        glasses.style.left = `${value*0.5}px`;
+        eraser.style.top = `${value*0.5}px`;
+        coffie.style.top = `${value*0.8}px`;
+        coffie.style.left = `-${value*0.15}px`;
+        bottomImg.style.top = `${value*0.15}px`;
+        textTitle.style.top = `${value*0.8}px`;
     });
 
     startPageBtn.onclick = () => {
