cde07896eb03030c364c96625383763dfd4c584e
VeriSkas <92_sveto4ka@mail.ru>
2021-08-25 17:33:39 +0300

    546 and fixed bug search+subtask

--- src/shared/constants/regexp.js
+++ src/shared/constants/regexp.js
@@ -1,7 +1,7 @@
 export const REGEXP = {
     validPassword: /^[a-z0-9а-я]{6,32}$/i,
     validEmail: /^[a-z0-9.!\-_+#%$‘&*/=?^`{|}~]{1,64}@[a-z0-9\-]{1,20}\.[a-z]{2,6}$/i,
-    validName: /^[a-z0-9а-я.\-_+#`~]{4,64}$/i,
+    validName: /^[a-z0-9а-я.\-_+#`~]{4,10}$/i,
     inputTodoLength: /^.{3,200}$/si,
     inputCommentLength: /^.{0,200}$/si,
     validListName: /^[a-zа-я0-9]{1,20}$/i,
