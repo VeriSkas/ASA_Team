36a14552ed6752a7fa5368a81563e5be022413b4
VeriSkas <92_sveto4ka@mail.ru>
2021-09-08 18:15:12 +0300

    sidebar close

--- src/shared/media.js
+++ src/shared/media.js
@@ -1,10 +1,10 @@
-export const checkMediaQuery = () => {
-    const sidebar = document.querySelector('.wrapper__content_sidebar');
+// export const checkMediaQuery = () => {
+//     const sidebar = document.querySelector('.wrapper__content_sidebar');
 
-    if (window.innerWidth < 1300) {
-        sidebar.classList.add('close');
-    } else {
-        sidebar.classList.remove('close');
-    }
-}
+//     if (window.innerWidth < 1300) {
+//         // sidebar.classList.add('close');
+//     } else {
+//         sidebar.classList.remove('close');
+//     }
+// }
 
